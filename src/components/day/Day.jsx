f6cfeadd4ab08b17c3ae58555f5374567f9c4315
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-01-10 21:23:08 +0200

    add current time line

--- src/components/day/Day.jsx
+++ src/components/day/Day.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Hour from '../hour/Hour';
+import moment from 'moment';
 
 import './day.scss';
 
 export default function Day({ dateInfoInNumbers, events }) {
+  const [top, setTop] = useState(0);
+
+  useEffect(() => {
+    let intervalId = setInterval(() => {
+      const [hours, minutes] = moment().format('H:m').split(':');
+      setTop(Number(hours) * 60 + Number(minutes));
+    }, 1000);
+    // return clearInterval(intervalId)
+  }, []);
+
   const hours = Array(24)
     .fill()
     .map((_, index) => index);
   return (
     <div className="calendar__day" data-day={`${dateInfoInNumbers}`}>
+      {dateInfoInNumbers === moment().format('DD.MM.YYYY') ? (
+        <div className="currentTimeLine" style={{ top: top + 'px' }}>
+          <div className="currentTimeLine__circle" />
+        </div>
+      ) : null}
       {hours.map((hour) => (
         <Hour
           key={hour}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,10 +10,10 @@ export default function Sidebar() {
   return (
     <div className="calendar__time-scale">
       {hours.map((hour) => (
-        <div className="time-slot" key={hour}>
+        <div className="time-slot" data-hour={hour} key={hour}>
           <span className="time-slot__time">{`${hour}:00`}</span>
         </div>
       ))}
     </div>
   );
-};
+}
