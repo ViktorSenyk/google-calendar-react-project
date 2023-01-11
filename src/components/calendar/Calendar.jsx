45c56bf5d02c8ac420a35a5725f700702bf39abe
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-01-11 17:56:41 +0200

    add modal window

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 
 import Navigation from './../navigation/Navigation';
 import Week from '../week/Week';
 import Sidebar from '../sidebar/Sidebar';
 
 import './calendar.scss';
+import Modal from '../modal/Modal';
 
-export default function Calendar({ weekStartDate }) {
+export default class Calendar extends Component {
+  state = {
+    modalWindowBoolean: false,
+    clickInfo: false,
+  };
+  setModalWindowBoolean = () =>
+    this.setState({ modalWindowBoolean: !this.state.modalWindowBoolean });
   // ======================================= days generator
-  const currentWeekRangeInfoGenerator = () => {
-    const weekStartDayCopy = weekStartDate.clone();
+  currentWeekRangeInfoGenerator = () => {
+    const weekStartDayCopy = this.props.weekStartDate.clone();
     const currentWeekInfoArr = [weekStartDayCopy.format('DD.MM.YYYY dddd')];
 
     for (let i = 0; i < 6; i += 1) {
@@ -19,30 +26,94 @@ export default function Calendar({ weekStartDate }) {
     }
     return currentWeekInfoArr;
   };
-  // ======================================= get info from element when was click
-  const getInfoFromClick = (e) => {
-    if (e.target.dataset.hour)
-      console.log({
-        hour: e.target.dataset.hour,
-        day: e.target.closest('.calendar__day').dataset.day,
+  // ======================================= get time info from click
+  getInfoFromClick = (e) => {
+    if (e.target.dataset.minutes) {
+      const [day, month, year] = e.target
+        .closest('.calendar__day')
+        .dataset.day.split('.');
+      const chosenHour = e.target.closest('.calendar__time-slot').dataset.hour;
+      const chosenMinutes = e.target.dataset.minutes;
+      this.setState({
+        modalWindowBoolean: !this.state.modalWindowBoolean,
+        clickInfo: {
+          day: `${year}-${month}-${day}`,
+          hour: chosenHour < 10 ? '0' + chosenHour : chosenHour,
+          minutes: chosenMinutes < 10 ? '0' + chosenMinutes : chosenMinutes,
+        },
       });
-    return {
-      hour: e.target.dataset.hour,
-      day: e.target.closest('.calendar__day').dataset.day,
-    };
+    }
   };
   // ======================================= currentWeekInfo = ['DD.MM.YYYY dddd', 'DD.MM.YYYY dddd' ... + 5]
-  const currentWeekInfo = currentWeekRangeInfoGenerator();
+  currentWeekInfo = this.currentWeekRangeInfoGenerator();
   // ======================================= render: Navigation, Sidebar, Week
-  return (
+  render = () => (
     <section className="calendar">
-      <Navigation currentWeekInfo={currentWeekInfo} />
-      <div className="calendar__body" onClick={(e) => getInfoFromClick(e)}>
+      <Modal
+        onOffBoolean={this.state.modalWindowBoolean}
+        setOnOffBoolean={this.setModalWindowBoolean}
+        clickInfo={this.state.clickInfo}
+      />
+      <Navigation currentWeekInfo={this.currentWeekInfo} />
+      <div className="calendar__body" onClick={(e) => this.getInfoFromClick(e)}>
         <div className="calendar__week-container">
           <Sidebar />
-          <Week currentWeekInfo={currentWeekInfo} />
+          <Week currentWeekInfo={this.currentWeekInfo} />
         </div>
       </div>
     </section>
   );
 }
+
+// export default function Calendar({ weekStartDate }) {
+//   const [modalWindowBoolean, setModalWindowBoolean] = useState(false);
+//   const [clickInfo, setClickInfo] = useState({});
+//   console.log(clickInfo);
+//   // ======================================= days generator
+//   const currentWeekRangeInfoGenerator = () => {
+//     const weekStartDayCopy = weekStartDate.clone();
+//     const currentWeekInfoArr = [weekStartDayCopy.format('DD.MM.YYYY dddd')];
+
+//     for (let i = 0; i < 6; i += 1) {
+//       currentWeekInfoArr.push(
+//         weekStartDayCopy.add(1, 'd').format('DD.MM.YYYY dddd')
+//       );
+//     }
+//     return currentWeekInfoArr;
+//   };
+//   // ======================================= get time info from click
+//   const getInfoFromClick = (e) => {
+//     if (e.target.dataset.minutes) {
+//       const [day, month, year] = e.target
+//         .closest('.calendar__day')
+//         .dataset.day.split('.');
+//       const chosenHour = e.target.closest('.calendar__time-slot').dataset.hour;
+//       const chosenMinutes = e.target.dataset.minutes;
+//       setModalWindowBoolean(!modalWindowBoolean);
+//       setClickInfo({
+//         day: `${year}-${month}-${day}`,
+//         hour: chosenHour < 10 ? '0' + chosenHour : chosenHour,
+//         minutes: chosenMinutes < 10 ? '0' + chosenMinutes : chosenMinutes,
+//       });
+//     }
+//   };
+//   // ======================================= currentWeekInfo = ['DD.MM.YYYY dddd', 'DD.MM.YYYY dddd' ... + 5]
+//   const currentWeekInfo = currentWeekRangeInfoGenerator();
+//   // ======================================= render: Navigation, Sidebar, Week
+//   return (
+//     <section className="calendar">
+//       <Modal
+//         onOffBoolean={modalWindowBoolean}
+//         setOnOffBoolean={setModalWindowBoolean}
+//         clickInfo={clickInfo}
+//       />
+//       <Navigation currentWeekInfo={currentWeekInfo} />
+//       <div className="calendar__body" onClick={(e) => getInfoFromClick(e)}>
+//         <div className="calendar__week-container">
+//           <Sidebar />
+//           <Week currentWeekInfo={currentWeekInfo} />
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,50 +1,130 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 import './modal.scss';
 
-class Modal extends Component {
-  render() {
-    return (
-      <div className="modal overlay">
-        <div className="modal__content">
-          <div className="create-event">
-            <button className="create-event__close-btn">+</button>
-            <form className="event-form">
+// export default class Modal extends Component {
+//   state = {
+//     title: '',
+//     startDate: this.props.clickInfo.day,
+//   };
+//   render = () => {
+//     console.log(this.props.clickInfo.day);
+//     console.log(this.state.startDate);
+//     return (
+//       <div
+//         className="modal overlay"
+//         style={{ display: this.props.onOffBoolean ? 'flex' : 'none' }}
+//       >
+//         <div className="modal__content">
+//           <div className="create-event">
+//             <button
+//               className="create-event__close-btn"
+//               onClick={() =>
+//                 this.props.setOnOffBoolean(!this.props.onOffBoolean)
+//               }
+//             >
+//               +
+//             </button>
+//             <form className="event-form">
+//               <input
+//                 type="text"
+//                 name="title"
+//                 placeholder="Write your title..."
+//                 className="event-form__field"
+//                 value={this.state.title}
+//                 onChange={(e) => this.setState({ title: e.target.value })}
+//               />
+//               <div className="event-form__time">
+//                 <input
+//                   type="date"
+//                   name="date"
+//                   className="event-form__field"
+//                   value={this.state.startDate}
+//                   onChange={(e) => this.setState({ startDate: e.target.value })}
+//                 />
+//                 <input
+//                   type="time"
+//                   name="startTime"
+//                   className="event-form__field"
+//                 />
+//                 <span>-</span>
+//                 <input
+//                   type="time"
+//                   name="endTime"
+//                   className="event-form__field"
+//                 />
+//               </div>
+//               <textarea
+//                 name="description"
+//                 placeholder="Description"
+//                 className="event-form__field"
+//               ></textarea>
+//               <button type="submit" className="event-form__submit-btn">
+//                 Create
+//               </button>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   };
+// }
+
+export default function Modal({ onOffBoolean, setOnOffBoolean, clickInfo }) {
+  const [title, setTitle] = useState('');
+  const [startDate, setStartDate] = useState(clickInfo ? clickInfo.day : '2023-11-17');
+  console.log(clickInfo.day);
+  console.log(startDate);
+
+  return (
+    <div
+      className="modal overlay"
+      style={{ display: onOffBoolean ? 'flex' : 'none' }}
+    >
+      <div className="modal__content">
+        <div className="create-event">
+          <button
+            className="create-event__close-btn"
+            onClick={() => setOnOffBoolean(!onOffBoolean)}
+          >
+            +
+          </button>
+          <form className="event-form">
+            <input
+              type="text"
+              name="title"
+              placeholder="Write your title..."
+              className="event-form__field"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <div className="event-form__time">
               <input
-                type="text"
-                name="title"
-                placeholder="Title"
+                type="date"
+                name="date"
+                className="event-form__field"
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+              <input
+                type="time"
+                name="startTime"
                 className="event-form__field"
               />
-              <div className="event-form__time">
-                <input type="date" name="date" className="event-form__field" />
-                <input
-                  type="time"
-                  name="startTime"
-                  className="event-form__field"
-                  onChange={this.handleChange}
-                />
-                <span>-</span>
-                <input
-                  type="time"
-                  name="endTime"
-                  className="event-form__field"
-                />
-              </div>
-              <textarea
-                name="description"
-                placeholder="Description"
-                className="event-form__field"
-              ></textarea>
-              <button type="submit" className="event-form__submit-btn">
-                Create
-              </button>
-            </form>
-          </div>
+              <span>-</span>
+              <input type="time" name="endTime" className="event-form__field" />
+            </div>
+            <textarea
+              name="description"
+              placeholder="Description"
+              className="event-form__field"
+            ></textarea>
+            <button type="submit" className="event-form__submit-btn">
+              Create
+            </button>
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Modal;
