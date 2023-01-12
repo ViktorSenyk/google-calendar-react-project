6bd3506b406cc13db639cf64f38a8fc3131f289f
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-01-12 09:38:37 +0200

    + fetch fix (error)

--- src/gateway/tasksGateway.js
+++ src/gateway/tasksGateway.js
@@ -1,9 +1,13 @@
 const baseUrl = 'https://6384c3013fa7acb14fffc5eb.mockapi.io/tasks';
 
 export const updateFromServer = () =>
-  fetch(baseUrl).then((res) =>
-    res.ok ? res.json() : new Error('Network Error')
-  );
+  fetch(baseUrl).then((res) => {
+    if (!res.ok) {
+      alert("Internal Server Error. Can't display events");
+      return [];
+    }
+    return res.json();
+  });
 
 export const onCreateTask = (taskInfo) =>
   fetch(baseUrl, {
@@ -13,12 +17,18 @@ export const onCreateTask = (taskInfo) =>
     },
     body: JSON.stringify(taskInfo),
   }).then((res) => {
-    if (!res.ok) throw new Error('Network error');
+    if (!res.ok) {
+      alert("Internal Server Error. Can't display events");
+      throw new Error('Network error');
+    }
   });
 
 export const onDeleteTask = (id) =>
   fetch(`${baseUrl}/${id}`, {
     method: 'DELETE',
   }).then((res) => {
-    if (!res.ok) throw new Error('Network error');
+    if (!res.ok) {
+      alert("Internal Server Error. Can't display events");
+      throw new Error('Network error');
+    }
   });
